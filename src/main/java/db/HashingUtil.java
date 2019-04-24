09fe5a947024d5599b028efeb6dba629fa160e98
stamo15 <stamo15@alustudent.com>
2019-04-24 21:52:14 +0400

    Creating HashingUtil class to hash passwords

---
+++ src/main/java/db/HashingUtil.java
@@ -0,0 +1,36 @@
+package db;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class HashingUtil {
+    //https://stackoverflow.com/questions/33085493/how-to-hash-a-password-with-sha-512-in-java
+    public static String getHashedPassword(String passwordToHash, String salt){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(salt.getBytes(StandardCharsets.UTF_8));
+            byte[] bytes = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++){
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+
+    public static String getNextSalt() {
+        byte[] saltBytes = new byte[16];
+        Random random = new SecureRandom();
+        random.nextBytes(saltBytes);
+        return new String(saltBytes);
+    }
+
+}
